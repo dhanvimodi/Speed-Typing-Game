12b5061e58e3dedb7e1478dd5965ab689fee1e43
dhanvimodi <dhanvi.modi@gmail.com>
2023-01-18 13:14:50 +0530

    basic structure of app and styles added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Challenge: build the basic structure of our game
+ * 
+ * 1. <h1> title at the top
+ * 2. <textarea> for the box to type in 
+ *      (tip: React normalizes <textarea /> to be more like <input />, 
+ *      so it can be used as a self-closing element and uses the `value` property
+ *      to set its contents)
+ * 3. <h4> ti display the amount of time remaining
+ * 4. <button> to start the game
+ * 5. Another <h1> to display the word count
+ * 
+ */
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+     const [text,setText]=React.useState("")
+     const [timeRemaining,setTimeRemaining]=React.useState(5)
+    
+    function handleChange(event){
+        const {name,value}=event.target
+        setText(value)
+    }
+   console.log(text)
+    return(
+        <div>
+            <h1>Speed Typing Game</h1>
+             <textarea
+            value={text}
+             placeholder="You can start typing here"
+             type="text"
+             onChange={handleChange}
+            />
+            <h4>Time Remaining {timeRemaining}</h4>
+            <button>Start game</button>
+            <h1>Word count</h1>
+        </div>
+    )
 }
-
-export default App;
+export default App
