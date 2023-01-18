b0006154b989215d20f22cf3b97382e3256738d4
dhanvimodi <dhanvi.modi@gmail.com>
2023-01-18 16:04:25 +0530

    all major functionality completed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
-/**
- * Challenge: build the basic structure of our game
- * 
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in 
- *      (tip: React normalizes <textarea /> to be more like <input />, 
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- * 
- */
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
-function App(){
-     const [text,setText]=React.useState("")
-     const [timeRemaining,setTimeRemaining]=React.useState(5)
-    
-    function handleChange(event){
-        const {name,value}=event.target
-        setText(value)
+function App() {
+
+  const  STARTING_TIME=5
+
+  const [timeRemaining, setTimeRemaining] = useState()
+  const [text, setText] = useState("")
+  const [wordCount, setWordCount] = useState()
+  const [isGameStarted,setIsGameStarted]=useState(false)
+
+  const textRef=useRef()
+
+useEffect(() => {
+  if(timeRemaining > 0) {
+      setTimeout(() => {
+          setTimeRemaining(time => time - 1)
+      }, 1000)
+  }
+  else{
+    handleEndGame()
+  }
+}, [timeRemaining])
+
+  function handleChange(e) {
+    const { value } = e.target
+    if(isGameStarted){
+      setText(value)
     }
-   console.log(text)
-    return(
-        <div>
-            <h1>Speed Typing Game</h1>
-             <textarea
-            value={text}
-             placeholder="You can start typing here"
-             type="text"
-             onChange={handleChange}
-            />
-            <h4>Time Remaining {timeRemaining}</h4>
-            <button>Start game</button>
-            <h1>Word count</h1>
-        </div>
-    )
+  }
+
+  function handleGameStart(event){
+    
+    setIsGameStarted(true)
+    setText("")
+    setTimeRemaining(STARTING_TIME)
+    setWordCount()
+    textRef.current.disabled=false
+    textRef.current.focus()
+  }
+
+  function handleEndGame(){
+    setIsGameStarted(false)
+    countWords()
+  }
+  function countWords() {
+    setWordCount(text.trim().split(" ")
+                    .filter(w=>w!=='')
+                    .length)
+  }
+
+  return (
+    <div>
+      <h1>Speed Typing Game</h1>
+      <textarea
+        disabled={!isGameStarted}
+        ref={textRef}
+        onChange={handleChange}
+        value={text}
+      />
+      <h4>Time Remaining {timeRemaining}</h4>
+      <button disabled={isGameStarted} onClick={!isGameStarted?handleGameStart:null}>Start game</button>
+      <h1>Word count {wordCount}</h1>
+    </div>
+  )
 }
 export default App
